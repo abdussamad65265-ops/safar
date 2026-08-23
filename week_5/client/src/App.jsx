b5198dac6612b42954a0e72805c38d1c42e04829
safar <abdussamad65265@gmail.com>
2026-08-23 23:54:27 +0530

    Create App component with routing and navbar

---
+++ week_5/client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
